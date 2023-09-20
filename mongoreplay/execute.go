bce7ec029817ad7e538172008fccdd1bdeb1c8de
kyontan <kyontan@monora.me>
2023-09-21 02:56:06 +0900

    add debug

--- mongoreplay/execute.go
+++ mongoreplay/execute.go
@@ -269,7 +269,7 @@ func (context *ExecutionContext) Execute(op *RecordedOp, socket *mgo.MongoSocket
 
 		if err != nil {
 			context.CursorIDMap.MarkFailed(op)
-			return opToExec, reply, fmt.Errorf("error executing op: %v", err)
+			return opToExec, reply, fmt.Errorf("error executing op: %+v, err: %v", op, err)
 		}
 		if reply != nil {
 			context.AddFromWire(reply, op)
